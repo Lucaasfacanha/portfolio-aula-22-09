237984cb3e0792280ffa077d34750581ef618175
lucaasfacanha <lucaspereirafacanha@gmail.com>
2023-10-16 16:06:33 -0300

    adicionando efeito menu mobile java

---
+++ menu.js
@@ -0,0 +1,15 @@
+let botaoAbrirMenu = document.getElementById('botaoAbrirMenu')
+let menu = document.getElementById('menuMobile2')
+let overlay = document.getElementById('overlayMenu')
+
+botaoAbrirMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrirMenu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrirMenu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrirMenu')
+})
