a2d21d035ada8338847d0038c67ceb5bf409f912
lucaasfacanha <lucaspereirafacanha@gmail.com>
2023-11-24 08:25:15 -0300

    ecluindo um js

--- main.js
+++ main.js
@@ -1,44 +1,17 @@
 import './style.css'
 
-const salas = [
-    {
-      turma: "A",
-      alunos: [{ nota: 10 }, { nota: 7 }, { nota: 1 }],
-    },
-    {
-      turma: "B",
-      alunos: [{ nota: 8 }, { nota: 10 }, { nota: 7.5 }, { nota: 10 }, { nota: 2 }, { nota: 3 }],
-    },
-    {
-      turma: "C",
-      alunos: [{ nota: 4 }, { nota: 6 }, { nota: 7.5 }, { nota: 10 }, { nota: 5 }],
-    },
-  ];
+let botaoAbrirMenu = document.getElementById('botaoAbrirMenu')
+let menu = document.getElementById('menuMobile2')
+let overlay = document.getElementById('overlayMenu')
 
-  function getMediaTurmas() {
-    return salas.reduce((acc, item) => {
-      const somaNotas = item.alunos.reduce((acc, item) => {
-        return acc + item.nota;
-      }, 0);
-      const media = somaNotas / item.alunos.length;
-      return { ...acc, [item.turma]: media };
-    }, {});
-  }
-  
-  
-  function main() {
-    const mediaSalas = getMediaTurmas();
-    const salasComMaisInformacoes = salas.map((sala) => {
-      return { ...sala, media: mediaSalas[sala.turma] };
-    });
-    const turmasOrdenadas = salasComMaisInformacoes.sort((primeiraSala, segundaSala) => {
-      if (primeiraSala.media > segundaSala.media) {
-        return -1;
-      }
-  
-      return 1;
-    });
-  
-    console.log(turmasOrdenadas);
-  }
-main()
+botaoAbrirMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrirMenu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrirMenu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrirMenu')
+})
